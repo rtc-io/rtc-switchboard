75704af1071cd962e995703bb3967fc192844ab3
Damon Oehlman <damon.oehlman@gmail.com>
2013-11-12 09:13:42 +1100

    Added mutisignaller test logic - closes #5

--- test/connect.js
+++ test/connect.js
@@ -3,6 +3,7 @@ var signaller = require('rtc-signaller');
 
 var start = module.exports = function(test, board) {
   var socket;
+  var signallers = [];
 
   test('create a socket', function(t) {
     t.plan(2);
@@ -12,16 +13,51 @@ var start = module.exports = function(test, board) {
 
   test('wrap a signaller around the socket', function(t) {
     var sig;
+    var spark;
 
-    t.plan(2);
+    t.plan(6);
     board.once('announce', function(data) {
       t.equal(sig.id, data.id, 'announced signaller');
+
+      // validate that the spark is mapped to the peer id
+      t.ok(spark = board.sparks.get(data.id), 'got spark');
+
+      // spark has the peer listed in
+      t.ok(Array.isArray(spark.peers), 'spark has a peer list');
+      t.equal(spark.peers.length, 1);
+      t.ok(spark.peers.indexOf(data.id) >= 0, 'spark associated with peer');
     });
 
     // create the signaller instance
     t.ok(sig = signaller(socket), 'signaller created');
 
     // announce
+    signallers.push(sig);
+    sig.announce();
+  });
+
+  test('add an additional signaller to the socket', function(t) {
+    var sig;
+    var spark;
+
+    t.plan(6);
+    board.once('announce', function(data) {
+      t.equal(sig.id, data.id, 'announced signaller');
+
+      // validate that the spark is mapped to the peer id
+      t.ok(spark = board.sparks.get(data.id), 'got spark');
+
+      // spark has the peer listed in
+      t.ok(Array.isArray(spark.peers), 'spark has a peer list');
+      t.equal(spark.peers.length, 2);
+      t.ok(spark.peers.indexOf(data.id) >= 0, 'spark associated with peer');
+    });
+
+    // create the signaller instance
+    t.ok(sig = signaller(socket), 'signaller created');
+
+    // announce
+    signallers.push(sig);
     sig.announce();
   });
 
