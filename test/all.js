531531d6e7bc78de679dd0b057dd4ba55dcb440f
Damon Oehlman <damon.oehlman@gmail.com>
2013-12-18 16:04:21 +1100

    Include room-info test

--- test/all.js
+++ test/all.js
@@ -5,5 +5,6 @@ server.start(function(test, board) {
   require('./broadcast-by-default')(test, board);
   require('./room-isolation')(test, board);
   require('./room-leave')(test, board);
+  require('./room-info')(test, board);
   require('./to-messaging')(test, board);
 });
