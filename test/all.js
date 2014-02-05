a7fffe877402c78706989e0337ea9660260e66b5
Damon Oehlman <damon.oehlman@gmail.com>
2014-02-05 15:45:15 +1100

    Removed broadcast-by-default tests as they lacked relevance

--- test/all.js
+++ test/all.js
@@ -2,7 +2,6 @@ var server = require('./helpers/server');
 
 server.start(function(test, board) {
   require('./connect')(test, board);
-  require('./broadcast-by-default')(test, board);
   require('./room-isolation')(test, board);
   require('./room-leave')(test, board);
   require('./room-info')(test, board);
