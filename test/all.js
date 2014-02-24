e8571914d3327a872143cf8284c4bfd52b619c40
Damon Oehlman <damon.oehlman@gmail.com>
2014-02-24 17:54:36 +1100

    Include room-events test

--- test/all.js
+++ test/all.js
@@ -6,5 +6,6 @@ server.start(function(test, board) {
   require('./room-leave')(test, board);
   require('./room-info')(test, board);
   require('./room-changes')(test, board);
+  require('./room-events')(test, board);
   require('./to-messaging')(test, board);
 });
