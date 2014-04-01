613ef610132c7fe009afdc3cd430de60f7d771cb
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-02 05:51:06 +1100

    Started adding logic to decode JSONified strings (for backwards compatibility)

--- parser-noop.js
+++ parser-noop.js
@@ -5,5 +5,16 @@ exports.encoder = function encoder(data, fn) {
 };
 
 exports.decoder = function decoder(data, fn) {
+  // var isString = typeof data == 'string' || (data instanceof String);
+  // var firstChar = isString && data.length > 1 && data.charAt(0);
+  // var lastChar = isString && data.length > 1 && data.charAt(data.length - 1);
+
+  // if (! isString) {
+  //   return fn(null, data);
+  // }
+
+  // // if we have been passed what appears to be a JSONified string
+  // // then parse it.  This is required for 
+
   fn(null, data);
 };
