dad07dabaa91942f25b2488f616b008100ec528a
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-11 22:27:02 +1000

    Redirect http requests for / to github

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var server = require('http').createServer();
 var switchboard = require('./')(server, { servelib: true });
 var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || 3000;
 
+server.on('request', function(req, res) {
+  if (req.url === '/') {
+    res.writeHead(302, {
+      'Location': 'https://github.com/rtc-io/rtc-switchboard'
+    });
+    res.end('switchboard available from: https://github.com/rtc-io/rtc-switchboard');
+  }
+});
+
 // start the server
 server.listen(port, function(err) {
   if (err) {
