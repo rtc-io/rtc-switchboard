22687322c53054a02cf0ec9911b1df87a438a66e
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-30 10:18:57 +1000

    Use replify to allow us to inspect status of the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var server = require('http').createServer();
 var switchboard = require('./')(server, { servelib: true });
 var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || 3000;
+var replify = require('replify');
 
 server.on('request', function(req, res) {
   if (req.url === '/') {
@@ -20,6 +21,19 @@ server.listen(port, function(err) {
   console.log('server running at http://localhost:' + port + '/');
 });
 
+// add the repl
+replify({
+  name: 'switchboard',
+  app: switchboard,
+  contexts: {
+    server: server
+  }
+});
+
+switchboard.on('room:create', function(room) {
+  console.log('room ' + room + ' created, now have ' + switchboard.rooms.length + ' active rooms');
+});
+
 switchboard.on('room:destroy', function(room) {
   console.log('room ' + room + ' destroyed, ' + switchboard.rooms.length + ' active rooms remain');
 
@@ -28,3 +42,4 @@ switchboard.on('room:destroy', function(room) {
     gc();
   }
 });
+
