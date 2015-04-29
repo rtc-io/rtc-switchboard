a80db239d46e05dcecc628cc1275c2afa87b92a0
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-29 22:52:05 +1000

    Disable the repl

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var server = require('http').createServer();
 var switchboard = require('./')(server, { servelib: true });
 var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || 3000;
-var replify = require('replify');
+// var replify = require('replify');
 
 server.on('request', function(req, res) {
   if (req.url === '/') {
@@ -21,25 +21,24 @@ server.listen(port, function(err) {
   console.log('server running at http://localhost:' + port + '/');
 });
 
-// add the repl
-replify({
-  name: 'switchboard',
-  app: switchboard,
-  contexts: {
-    server: server
-  }
-});
-
-switchboard.on('room:create', function(room) {
-  console.log('room ' + room + ' created, now have ' + switchboard.rooms.length + ' active rooms');
-});
-
-switchboard.on('room:destroy', function(room) {
-  console.log('room ' + room + ' destroyed, ' + switchboard.rooms.length + ' active rooms remain');
-
-  if (typeof gc == 'function') {
-    console.log('gc');
-    gc();
-  }
-});
-
+// // add the repl
+// replify({
+//   name: 'switchboard',
+//   app: switchboard,
+//   contexts: {
+//     server: server
+//   }
+// });
+//
+// switchboard.on('room:create', function(room) {
+//   console.log('room ' + room + ' created, now have ' + switchboard.rooms.length + ' active rooms');
+// });
+//
+// switchboard.on('room:destroy', function(room) {
+//   console.log('room ' + room + ' destroyed, ' + switchboard.rooms.length + ' active rooms remain');
+//
+//   if (typeof gc == 'function') {
+//     console.log('gc');
+//     gc();
+//   }
+// });
